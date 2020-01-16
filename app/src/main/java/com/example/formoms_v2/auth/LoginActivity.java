922c15f44acb603c69d4711e926f25bf08b0cbde
refokes <refokes@gmial.com>
2020-01-16 09:15:00 +0700

    login register firebase

--- app/src/main/java/com/example/formoms_v2/auth/LoginActivity.java
+++ app/src/main/java/com/example/formoms_v2/auth/LoginActivity.java
@@ -1,16 +1,96 @@
 package com.example.formoms_v2.auth;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.formoms_v2.MainActivity;
 import com.example.formoms_v2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
+    private EditText mEmailField;
+    private EditText mPasswordField;
+    private Button mloginBtn,mregisterBtn;
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_login);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
+        mEmailField = findViewById(R.id.email_login);
+        mPasswordField = findViewById(R.id.password_login);
+        mloginBtn = findViewById(R.id.btn_login);
+        mAuth = FirebaseAuth.getInstance();
+
+        mregisterBtn =(Button) findViewById(R.id.btn_regist);
+        mregisterBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+            }
+        });
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+
+            @Override
+
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+
+                if (firebaseAuth.getCurrentUser() != null) {
+                    startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                }
+            }
+        };
+
+        mloginBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+
+            public void onClick(View v) {
+                startSignIn();
+            }
+        });
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+
+    private void startSignIn() {
+        String email = mEmailField.getText().toString();
+        String password = mPasswordField.getText().toString();
+
+        if (TextUtils.isEmpty(email) | TextUtils.isEmpty(password)) {
+            Toast.makeText(LoginActivity.this, "Fields are Empty", Toast.LENGTH_SHORT).show();
+        } else {
+            mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener() {
+                @Override
+
+                public void onComplete(@NonNull Task task) {
+                    if (!task.isSuccessful()) {
+                        Toast.makeText(LoginActivity.this, "Login Problem", Toast.LENGTH_SHORT).show();
+                    }else{
+                        startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                    }
+                }
+            });
+        }
     }
 }
+
--- app/src/main/java/com/example/formoms_v2/auth/RegisterActivity.java
+++ app/src/main/java/com/example/formoms_v2/auth/RegisterActivity.java
@@ -1,16 +1,73 @@
 package com.example.formoms_v2.auth;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.formoms_v2.R;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    Button btn_regis;
+    EditText email_regis,password_regis;
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.layout_register);
+        btn_regis = (Button) findViewById(R.id.btn_regis);
+        email_regis= (EditText) findViewById(R.id.email_regis);
+        password_regis= (EditText) findViewById(R.id.password_regis);
+        mAuth = FirebaseAuth.getInstance();
+
+        btn_regis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String email = email_regis.getText().toString();
+                String password = password_regis.getText().toString();
+
+                if (email.isEmpty()){
+                    email_regis.setError("email kosong");
+                    email_regis.requestFocus();
+                    return;
+                }
+                if (password.isEmpty()){
+                    password_regis.setError("password kosong");
+                    password_regis.requestFocus();
+                    return;
+                }else if(password.length()<6){
+                    password_regis.setError("Minimal 6 digit password");
+                    password_regis.requestFocus();
+                    return;
+                }
+
+                mAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(RegisterActivity.this, "Berhasil", Toast.LENGTH_SHORT).show();
+                            //startActivity(new Intent(RegisterActivity.this,LoginActivity.class));
+                        }
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(RegisterActivity.this, "Register gagal", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+
+            }
+        });
     }
 }
+
